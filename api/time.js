8ea36817255965914ef33cfd330066bd1a8cf389
JKR <jcheruiyot11@gmail.com>
2022-08-03 10:16:35 +0300

    updated project for vercel deployment

---
+++ api/time.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  let date = new Date();
+  res.json({ unix: Math.round(date.getTime() / 1000), utc: date });
+});
+
+router.get("/:time", function (req, res) {
+  let time = req.params.time;
+  let date;
+
+  if (!Number.isNaN(parseInt(Number(time)))) {
+    date = new Date(Number(time));
+  } else {
+    date = new Date(time);
+  }
+
+  if (date == "Invalid Date") {
+    console.log(date, time, Number.isInteger(time));
+    res.json({ error: "Invalid date" });
+  } else {
+    res.json({ unix: date.getTime(), utc: date });
+  }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 // where your node app starts
 
 // init project
-var express = require("express");
-var app = express();
+const express = require("express");
+const time = require("./api/time");
 
+const app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require("cors");
@@ -12,6 +13,8 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
+
+app.use("/api/", time);
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
@@ -23,30 +26,7 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
-app.get("/api/:time", function (req, res) {
-  let time = req.params.time;
-  let date;
-
-  if (!Number.isNaN(parseInt(Number(time)))) {
-    date = new Date(Number(time));
-  } else {
-    date = new Date(time);
-  }
-
-  if (date == "Invalid Date") {
-    console.log(date, time, Number.isInteger(time));
-    res.json({ error: "Invalid date" });
-  } else {
-    res.json({ unix: date.getTime(), utc: date });
-  }
-});
-
-app.get("/api/", function (req, res) {
-  let date = new Date();
-  res.json({ unix: Math.round(date.getTime() / 1000), utc: date });
-});
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 8080, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
